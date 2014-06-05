0c21e96129e50537afe97f903efd4d3a8219c056
Brian Northan <bnorthan@gmail.com>
2014-06-05 11:10:46 -0400

    initial commit

---
+++ src/main/java/com/deconware/ops/fft/RichardsonLucy.java
@@ -0,0 +1,40 @@
+package com.deconware.ops.fft;
+
+import net.imagej.ops.Contingent;
+import net.imagej.ops.Op;  
+import net.imagej.ops.AbstractFunction;
+
+import net.imglib2.RandomAccessibleInterval;
+
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Op.class, name = "RichardsonLucy")
+public class RichardsonLucy<T extends RealType<T>, S extends RealType<S>> 
+		extends AbstractFunction<RandomAccessibleInterval<T>, RandomAccessibleInterval<S>> 
+		implements Op, Contingent
+{
+	@Parameter
+	private RandomAccessibleInterval<T> kernel;
+	
+	@Override
+	public RandomAccessibleInterval<S> compute(RandomAccessibleInterval<T> input,
+		RandomAccessibleInterval<S> output)
+		{
+			System.out.println("Richardson Lucy");
+			
+			System.out.println("input dim: "+input.dimension(0));
+			System.out.println("output dim: "+output.dimension(1));
+			System.out.println("psf dim: "+kernel.dimension(0));
+			
+			return null;
+		}
+	
+	@Override
+	public boolean conforms() 
+	{
+		return true;
+	}
+}
