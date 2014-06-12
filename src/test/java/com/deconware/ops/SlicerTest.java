3df1168c54a7896b787d2598688b17ad9e0026b1
Brian Northan <bnorthan@gmail.com>
2014-06-12 15:30:30 -0400

    added slice test

---
+++ src/test/java/com/deconware/ops/SlicerTest.java
@@ -0,0 +1,64 @@
+package com.deconware.ops;
+
+import net.imagej.ops.slicer.CroppedIterableInterval;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class SlicerTest extends AbstractOpsTest
+{
+	@Test
+	public void LoopThroughHyperSlicesTest()
+	{
+		int xSize=40;
+		int ySize=50;
+		int numChannels=3;
+		int numSlices=25;
+		int numTimePoints=5;
+		
+		Img<UnsignedByteType> testImage = generateUnsignedByteTestImg(true,
+				xSize, ySize, numChannels, numSlices, numTimePoints);
+				
+		int[] axisIndices=new int[3];
+		
+		// set up the axis so we loop through channels
+		axisIndices[0]=0;
+		axisIndices[1]=1;
+		axisIndices[2]=3;
+		
+		CroppedIterableInterval hyperSlices= new CroppedIterableInterval(ops, testImage,
+				axisIndices);
+		
+		Cursor<RandomAccessibleInterval<?>> c=hyperSlices.cursor();
+			
+		int numHyperSlices=0;
+		while(c.hasNext())
+		{
+			
+			c.fwd();
+			numHyperSlices++;
+			try
+			{
+				RandomAccessibleInterval<?> hyperSlice=c.get();
+				
+				Assert.assertEquals(3, hyperSlice.numDimensions());
+				Assert.assertEquals(hyperSlice.dimension(0), xSize);
+				Assert.assertEquals(hyperSlice.dimension(1), ySize);
+				Assert.assertEquals(hyperSlice.dimension(2), numSlices);
+				
+			}
+			catch (Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	
+		Assert.assertEquals(numChannels*numTimePoints, numHyperSlices);
+        
+	}
+}
